d47bcb598b7638563be5a2b1bfe60ab8d000a4ff
Ainesh Yarlagadda <ainesh@aineshs-mbp.lan>
2023-09-21 05:22:58 -0500

    draft spark code

---
+++ spark_kafka/src/streaming/spark_src_to_kafka.py
@@ -0,0 +1,31 @@
+import xmlschema
+from pyspark.sql.functions import *
+from pyspark.sql.types import BooleanType
+from pyspark.sql import SparkSession
+
+@udf(returnType=BooleanType())
+def xml_validation(file_name):
+    schema_file = "/Users/kyarl1/xsdfiles/sample_order.xsd"
+    my_schema = xmlschema.XMLSchema(schema_file)
+    return my_schema.is_valid(file_name)
+
+source_file_location = '/Users/kyarl1/xmlfiles/'
+
+spark = SparkSession.builder.appName("spark_src_to_kafka").getOrCreate()
+df = spark.read.format("xml").option("rowTag","Root").load(source_file_location)
+
+df1 = df.select("*", substring(input_file_name(),6,400).alias("fname"))
+
+df2 = df1.select("*", xml_validation('fname').alias('status'))
+
+final_df = df2.where(df2.status != False)
+
+
+final_df.writeStream\
+    .format("kafka")\
+    .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)\
+    .option("topic", KAFKA_TOPIC)\
+    .option("checkpointLocation", "/tmp/checkpoint")\
+    .trigger(once=True) \
+    .start()\
+    .awaitTermination()
